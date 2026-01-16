dd7f7c0e7b060a736339a019dbb710832c05959f
william94000schr <william.schneider94000@gmail.com>
2026-01-16 18:18:44 +0100

    correction proportion

--- src/train.py
+++ src/train.py
@@ -63,6 +63,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--num_classes', type=int, default=10, help='number of classes')
     parser.add_argument('--num_epochs', type=int, default=5, help='number of epochs')
-    parser.add_argument('--proportion', type=int, default=5, help='proportion of the original dataset')
+    parser.add_argument('--proportion', type=float, default=5, help='proportion of the original dataset')
     args = parser.parse_args()
     train(args.num_classes, args.num_epochs)
