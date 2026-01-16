cf1f364d888b6915224df7e0848206009a854c61
william94000schr <william.schneider94000@gmail.com>
2026-01-16 14:11:42 +0100

    Correction des poids pour FasterRCNN_v2

--- src/model.py
+++ src/model.py
@@ -5,7 +5,7 @@ from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
 def get_model(num_classes):
 
-    model = fasterrcnn_resnet50_fpn_v2(weights=FasterRCNN_ResNet50_FPN_Weights.DEFAULT)
+    model = fasterrcnn_resnet50_fpn_v2(weights=FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT)
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes + 1)
 
