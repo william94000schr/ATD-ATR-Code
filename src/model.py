5f0328e2a083b54cbee5ddee7ba77d134eba5402
william94000schr <william.schneider94000@gmail.com>
2026-01-21 14:21:53 +0100

    ajout de la validation

--- src/model.py
+++ src/model.py
@@ -7,7 +7,7 @@ def get_model(num_classes):
 
     model = fasterrcnn_resnet50_fpn_v2(weights=FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT, min_size = 128, max_size = 128)
     in_features = model.roi_heads.box_predictor.cls_score.in_features
-    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes + 1)
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
     return model
 
