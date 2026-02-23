b5541bc5303480dcde14ee82a6a0ddf5f3ce3ffa
dacher <mael.dacher@student.isae-supaero.fr>
2026-02-23 13:58:32 +0100

    debug

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,9 @@
-from .dataset import RadarCOCODataset
+from .dataset import SAR_ATR_Dataset
 from .transform import RadarTrainTransform, RadarValTransform
 from .utils import MLflowLogger, setup_output_dirs, print_dataset_stats
 
 __all__ = [
-    "RadarCOCODataset",
+    "SAR_ATR_Dataset",
     "RadarTrainTransform",
     "RadarValTransform",
     "MLflowLogger",
