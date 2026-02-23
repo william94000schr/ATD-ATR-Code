75492761f08da6d62017f6378906417856881737
william94000schr <william.schneider94000@gmail.com>
2026-02-23 22:44:23 +0100

    gradcam changement de layer encore3

--- src/gradcam.py
+++ src/gradcam.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 class FasterRCNNGradCAM:
     """GradCAM for Faster RCNN ResNet50 FPN — targets backbone.body.layer4."""
 
-    def __init__(self, model, target_layer_name="backbone.fpn.layer_blocks.3"):
+    def __init__(self, model, target_layer_name="backbone.body.layer4.2.conv3"):
         self.model = model
         self.activations = None
         self.gradients = None
